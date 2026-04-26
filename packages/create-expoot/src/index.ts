e82cd09f61693b4b421d2478f082a7cd2683fb87
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-04-27 00:05:25 +0900

    notes

--- packages/create-expoot/src/index.ts
+++ packages/create-expoot/src/index.ts
@@ -1,15 +1,24 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import toml from "toml";
+import { cwd } from "node:process";
 
-import { defineConfig } from "./define-config.ts";
+// This would be ideal to run against, but as the user won't have create-expoot
+// in their immediate node_modules, it won't give any Intellisense.
+//
+// Would JSON Schema be better? Or should we go back to TOML?
+// ... Or should we inline the types? Is that even possible with ArkType?
+await fs.writeFile(
+  path.resolve(cwd(), "create-expoot-config.mts"),
+  `
+import { defineConfig } from "create-expoot";
 
-const questionnaire = await fs.readFile(
-  path.resolve(import.meta.dirname, "questionnaire.toml"),
-  "utf-8",
+const config = defineConfig({
+  name: {
+    alphanumeric: "MyApp123",
+    display_name: "My App 123",
+    reverse_dns: "com.example.my-app-123",
+  },
+});
+export default config;
+`.trimStart(),
 );
-
-const parsed = toml.parse(questionnaire);
-// console.log(parsed);
-
-defineConfig(parsed);
