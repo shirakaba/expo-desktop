0941eb96b517a54c5e7032d362e47850c660331d
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-04-26 23:29:26 +0900

    support filling in partial config

--- packages/create-expoot/src/define-config.test.ts
+++ packages/create-expoot/src/define-config.test.ts
@@ -1,8 +1,28 @@
 import { expect, test } from "bun:test";
 
-import { badName } from "./fixtures/configs.ts";
+import { badName, defaultConfig } from "./fixtures/configs.ts";
 
 test("performs pattern validation on strings", () => {
   expect(badName.alphanumeric).toThrow();
   expect(badName.reverse_dns).toThrow();
+});
+
+test("fills in partial config", () => {
+  const { react_native_version, android, ios, macos, windows } = defaultConfig;
+
+  expect(react_native_version).toBe("0.82");
+
+  expect(android.application_id).toBe("com.example.my_app_123");
+  expect(android.package_namespace).toBe("com.example.my_app_123");
+  expect(android.root_project_name).toBe("My App 123");
+
+  expect(ios.bundle_display_name).toBe("My App 123");
+  expect(ios.bundle_identifier).toBe("com.example.my-app-123");
+
+  expect(macos.bundle_display_name).toBe("My App 123");
+  expect(macos.bundle_identifier).toBe("com.example.my-app-123");
+
+  expect(windows.display_name).toBe("My App 123");
+  expect(windows.namespace).toBe("com.example.myapp123");
+  expect(windows.project_name).toBe("MyApp123");
 });
--- packages/create-expoot/src/define-config.ts
+++ packages/create-expoot/src/define-config.ts
@@ -1,25 +1,66 @@
-import { type } from "arktype";
+import { ArkErrors, type } from "arktype";
 import { green, grey, yellow } from "kleur/colors";
 
-import { Questionnaire } from "./validate-questionnaire.ts";
+import { AppConfig, PartialAppConfig } from "./validate-app-config.ts";
 
-export function defineConfig(config: typeof Questionnaire.infer) {
-  const out = Questionnaire(config);
+export function defineConfig(config: typeof PartialAppConfig.infer) {
+  const partial = PartialAppConfig(config);
 
-  if (out instanceof type.errors) {
-    const summary = new Array<string>();
-    for (const [path, error] of Object.entries(out.byPath)) {
-      const actualIndex = error.problem.lastIndexOf(" (was ");
-      const problem =
-        actualIndex === -1
-          ? error.problem
-          : `${error.problem.startsWith("must be matched by ") ? `must be matched by ${green(error.problem.slice("must be matched by ".length, actualIndex))}` : error.problem.slice(0, actualIndex)}${grey(error.problem.slice(actualIndex))}`;
-
-      summary.push(`${yellow(path)} ${problem}`);
-    }
-
-    throw new Error(`Invalid config:\n${summary.join("\n")}`);
+  if (partial instanceof type.errors) {
+    throw new Error(`Invalid config:\n${makePrettySummary(partial).join("\n")}`);
   }
 
-  return out;
+  const {
+    name: { alphanumeric, display_name, reverse_dns },
+  } = partial;
+
+  const full = AppConfig({
+    ...partial,
+    // TODO: Dynamically grab latest minor version
+    react_native_version: "0.82",
+    android: {
+      application_id: reverse_dns.replaceAll("-", "_"),
+      package_namespace: reverse_dns.replaceAll("-", "_"),
+      root_project_name: display_name,
+      ...partial.android,
+    },
+    ios: {
+      bundle_display_name: display_name,
+      bundle_identifier: reverse_dns.replaceAll("_", "-"),
+      ...partial.ios,
+    },
+    macos: {
+      bundle_display_name: display_name,
+      bundle_identifier: reverse_dns.replaceAll("_", "-"),
+      ...partial.macos,
+    },
+    windows: {
+      display_name: display_name,
+      namespace: reverse_dns.replaceAll(/[\-_]/g, ""),
+      project_name: alphanumeric,
+      ...partial.windows,
+    },
+  } satisfies typeof PartialAppConfig.infer);
+
+  if (full instanceof type.errors) {
+    throw new Error(`Invalid config:\n${makePrettySummary(full).join("\n")}`);
+  }
+
+  return full;
+}
+
+function makePrettySummary({ byPath }: ArkErrors) {
+  const summary = new Array<string>();
+
+  for (const [path, error] of Object.entries(byPath)) {
+    const actualIndex = error.problem.lastIndexOf(" (was ");
+    const problem =
+      actualIndex === -1
+        ? error.problem
+        : `${error.problem.startsWith("must be matched by ") ? `must be matched by ${green(error.problem.slice("must be matched by ".length, actualIndex))}` : error.problem.slice(0, actualIndex)}${grey(error.problem.slice(actualIndex))}`;
+
+    summary.push(`${yellow(path)} ${problem}`);
+  }
+
+  return summary;
 }
