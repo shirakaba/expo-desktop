2cf627bee97980d04a931e78831b0b28721933e2
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-04-28 07:20:53 +0900

    good basis for CLI

--- packages/create-expoot/src/cli.ts
+++ packages/create-expoot/src/cli.ts
@@ -1,57 +1,34 @@
-import { intro, select } from "@clack/prompts";
-import cac from "cac";
+import { defineCommand, runMain } from "citty";
+import { default as kleur } from "kleur";
+import { grey } from "kleur/colors";
 
-const cli = cac("expoot");
-
-// TODO: Not impressed with cac. It uses `any` types and doesn't implement
-//       required args as advertised.
-//
-// May instead try:
-// - https://github.com/kazupon/gunshi
-// - https://github.com/unjs/citty
-// - https://github.com/privatenumber/cleye
-cli
-  .command("create-app", "Create a new Expo Desktop project")
-  .option("--alphanumeric <name>", "The name for the app in alphanumeric format")
-  .option("--display-name <name>", "The display name for the app")
-  .option("--reverse-dns <name>", "The reverse-DNS (Domain Name Specifier) for the app")
-  .action(async (positionals, options) => {
-    console.log("AHOY", positionals, options);
-    (
-      await import("./commands/add-expo-desktop-to-existing-expo-app.ts")
-    ).addExpoDesktopToExistingExpoApp();
-  });
-
-const parsed = cli.parse();
-
-console.log(JSON.stringify(parsed, null, 2));
-
-intro("create-expoot");
-
-const command = await select({
-  message: "What would you like to do?",
-  options: [
-    { value: "new-expo-desktop-project", label: "Create a new Expo Desktop project" },
-    {
-      value: "add-expo-desktop-to-existing-expo-app",
-      label: "Add Expo Desktop into an existing Expo app",
-    },
-    // In future:
-    // - Create a new standalone Expo Desktop module
-    // - Add an Expo Desktop module into an existing Expo app
-    // - Run prebuild?
-  ],
+const main = defineCommand({
+  meta: { name: "expoot", version: "1.0.0", description: "My Awesome CLI App" },
+  subCommands: {
+    "create-app": defineCommand({
+      meta: { name: "create-app", description: "Create a new Expo Desktop project" },
+      args: {
+        alphanumeric: {
+          type: "string",
+          description: `The ${kleur.bold("filesafe name")} for the app in alphanumeric format ${grey("(Example: 'MyApp123')")}`,
+          valueHint: "name",
+        },
+        "display-name": {
+          type: "string",
+          description: `The ${kleur.bold("display name")} for the app ${grey("(Examples: 'My App 123', '俺のアプリ')")}`,
+          valueHint: "name",
+        },
+        rdns: {
+          type: "string",
+          description: `The ${kleur.bold("reverse DNS")} for the app ${grey("(Example: 'com.example.my-app-123')")}`,
+          valueHint: "name",
+        },
+      },
+      async run({ args }) {
+        (await import("./commands/new-expo-desktop-project.ts")).newExpoDesktopProject(args);
+      },
+    }),
+  },
 });
 
-switch (command) {
-  case "add-expo-desktop-to-existing-expo-app": {
-    (
-      await import("./commands/add-expo-desktop-to-existing-expo-app.ts")
-    ).addExpoDesktopToExistingExpoApp();
-    break;
-  }
-  case "new-expo-desktop-project": {
-    (await import("./commands/new-expo-desktop-project.ts")).newExpoDesktopProject();
-    break;
-  }
-}
+await runMain(main);
--- packages/create-expoot/src/commands/new-expo-desktop-project.ts
+++ packages/create-expoot/src/commands/new-expo-desktop-project.ts
@@ -1,18 +1,69 @@
-import { taskLog, text, log } from "@clack/prompts";
+import { isCancel, text, log } from "@clack/prompts";
+import { default as kleur } from "kleur";
+import { grey, yellow } from "kleur/colors";
 
-export async function newExpoDesktopProject() {
-  // taskLog({ title: "Configuring app name" });
+export async function newExpoDesktopProject(args: {
+  alphanumeric: string | undefined;
+  "display-name": string | undefined;
+  rdns: string | undefined;
+}) {
+  log.info(
+    `🏎️  Running ${kleur.yellow("expoot create-app")}. Let's create a new Expo Desktop app in a subfolder!`,
+    { withGuide: false },
+  );
 
-  log.step("Configuring app name");
+  log.info(kleur.bold(kleur.inverse("  Configuring app name  ")), { withGuide: false });
 
-  const alphanumeric = await text({
-    message: "Please provide the name for the app in alphanumeric format.",
-    placeholder: "MyApp123",
-    initialValue: "MyApp",
-    validate(value) {
-      if (!value?.length) {
-        return "Must be at least one character long.";
-      }
-    },
-  });
+  let alphanumeric: Arg = args.alphanumeric;
+  if (!alphanumeric) {
+    alphanumeric = await text({
+      message: `Please provide the ${kleur.bold("filesafe name")} for the app in ${kleur.bold("alphanumeric")} format. ${grey("(Example: 'MyApp123')")}`,
+      placeholder: "MyApp",
+      initialValue: "MyApp",
+      validate(value) {
+        if (!value?.length) {
+          return "Must be at least one character long.";
+        }
+      },
+    });
+  }
+  if (isCancel(alphanumeric)) {
+    process.exit(0);
+  }
+
+  let displayName: Arg = args["display-name"];
+  if (!displayName) {
+    displayName = await text({
+      message: `Please provide the ${kleur.bold("display name")} for the app. ${grey("(Examples: 'My App 123', '俺のアプリ')")}`,
+      placeholder: "My App",
+      initialValue: "My App",
+      validate(value) {
+        if (!value?.length) {
+          return "Must be at least one character long.";
+        }
+      },
+    });
+  }
+  if (isCancel(displayName)) {
+    process.exit(0);
+  }
+
+  let rdns: Arg = args.rdns;
+  if (!rdns) {
+    rdns = await text({
+      message: `Please provide the ${kleur.bold("reverse DNS")} for the app. ${grey("(Example: 'com.example.my-app-123')")}`,
+      placeholder: "com.example.my-app",
+      initialValue: "com.example.my-app",
+      validate(value) {
+        if (!value?.length) {
+          return "Must be at least one character long.";
+        }
+      },
+    });
+  }
+  if (isCancel(rdns)) {
+    process.exit(0);
+  }
 }
+
+type Arg = string | symbol | undefined;
