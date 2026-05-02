0767d71260334806f99b57441153013ae26643c1
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2026-05-02 22:20:54 +0900

    update dep

--- packages/expo-desktop/src/create-app/create-expo-desktop-app.ts
+++ packages/expo-desktop/src/create-app/create-expo-desktop-app.ts
@@ -310,7 +310,7 @@ async function updatePackageJson({
     if (!packageJson.dependencies) {
       packageJson.dependencies = {};
     }
-    packageJson.dependencies["expo-desktop-config-plugins"] = "^1.0.0";
+    packageJson.dependencies["expo-desktop-config-plugins"] = "^1.1.0";
     packageJson.dependencies["react-native-macos"] = versions.macos;
     packageJson.dependencies["react-native-windows"] = versions.windows;
 
