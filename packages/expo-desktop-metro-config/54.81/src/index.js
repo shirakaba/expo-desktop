7a2d3d9f53db62b40b5603bb2188f52e11e95603
shirakaba <14055146+shirakaba@users.noreply.github.com>
2026-09-01 07:40:53 +0900

    add versioned metro-config package

---
+++ packages/expo-desktop-metro-config/54.81/src/index.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require("@expo/metro-config");
+const { makeMetroConfig: makeRnxKitMetroConfig } = require("@rnx-kit/metro-config");
+
+/**
+ * @param {Parameters<import("@expo/metro-config").getDefaultConfig>} args
+ * @return {ReturnType<import("@rnx-kit/metro-config").makeMetroConfig>}
+ */
+function makeMetroConfig(...args) {
+  const config = makeRnxKitMetroConfig(getDefaultConfig(...args));
+  config.resolver.platforms = ["ios", "android", "macos", "windows", "web"];
+  return config;
+}
+
+module.exports = makeMetroConfig;
