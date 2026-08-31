eac5e312b21a742139d9b7300b3a7280d004b0d5
shirakaba <14055146+shirakaba@users.noreply.github.com>
2026-08-31 21:31:41 +0900

    set config.resolver.platforms

--- templates/bare-minimum/54.81/metro.config.js
+++ templates/bare-minimum/54.81/metro.config.js
@@ -2,4 +2,5 @@ const { getDefaultConfig } = require("@expo/metro-config");
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
 
 const config = makeMetroConfig(getDefaultConfig(__dirname));
+config.resolver.platforms = ["ios", "android", "macos", "windows", "web"];
 module.exports = config;
